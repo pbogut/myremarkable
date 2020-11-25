f43bf2b63c1e11a02842cecfc46ae5163e042e78
Pawel Bogut <pbogut@pbogut.me>
2020-11-25 13:49:05 +0100

    Add copy files functionality

---
+++ RoboFile.php
@@ -0,0 +1,34 @@
+<?php
+
+class RoboFile extends \Robo\Tasks
+{
+    public function rmCopyFiles($opts = ['ssh-ip' => ''])
+    {
+        $sshIp = $opts['ssh-ip'];
+
+        $fileList = $this->getConfig('copy-files', []);
+        foreach ($fileList as $src => $dst) {
+            echo "$src -> $dst\n";
+            $this->taskExec('scp')
+                ->args([$src, "root@$sshIp:$dst"])
+                ->run();
+        }
+
+    }
+
+    protected function getConfig($key, $default = null)
+    {
+        $json = file_get_contents('configuration.json');
+        $data = json_decode($json, true);
+
+        $tmp = $data;
+        foreach (explode('.', $key) as $oneKey) {
+            if (!isset($tmp[$oneKey])) {
+                return $default;
+            }
+            $tmp = $tmp[$oneKey];
+        }
+
+        return $tmp ?? $default;
+    }
+}
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#=================================================
+# name:   install.sh
+# author: Pawel Bogut <https://pbogut.me>
+# date:   25/11/2020
+#=================================================
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $dir
+ssh_ip=${1:-$REMARKABLE_IP}
+
+usage() {
+  echo "$0 <remarkable_ip_address>"
+  exit 1
+}
+
+start_robo() {
+  robo rm:copy-files --ssh-ip=$ssh_ip
+}
+
+
+if [[ -z $ssh_ip ]]; then
+  usage
+fi
+
+start_robo
